9786995d4c051c869c18f51289153e07c029f384
Menyhárt Attila <menyhart.atika@gmail.com>
2022-12-02 11:28:37 +0100

    Day 1 - Calorie Counting

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace AdventOfCode;
+
+class Program
+{
+	public static void Main()
+	{
+		// Day 1 - Calorie Counting
+		Console.WriteLine(
+			$"The most calories an elf carries is {CalorieCounting.Answer()}."
+		);
+	}
+}
