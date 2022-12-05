bca20c5493e730440c1f75eeb38d0bd2070a8dd9
Menyhárt Attila <menyhart.atika@gmail.com>
2022-12-05 11:01:06 +0100

    Day 4 - Part 1 done

---
+++ src/CampCleanup/CampCleanup.cs
@@ -0,0 +1,25 @@
+namespace AdventOfCode;
+
+public class CampCleanup
+{
+	public static int PartOne()
+	{
+		int count = 0;
+		using StreamReader reader = new(
+			"src/CampCleanup/source.txt");
+		while (!reader.EndOfStream)
+		{
+			string[] m = reader.ReadLine().Split(',');
+			int[] assignmentOne = m[0].Split('-')
+				.Select(int.Parse).ToArray();
+			int[] assignmentTwo = m[1].Split('-')
+				.Select(int.Parse).ToArray();
+			if ((assignmentOne[0] <= assignmentTwo[0] &&
+				assignmentOne[1] >= assignmentTwo[1]) ||
+				(assignmentTwo[0] <= assignmentOne[0] &&
+				assignmentTwo[1] >= assignmentOne[1]))
+				count++;
+		}
+		return count;
+	}
+}
